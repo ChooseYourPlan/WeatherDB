20201235dd0df10b50a927bc9e0e1c35f7d934da
ChooseYourPlan <timweissenfels7@gmail.com>
2019-05-30 21:12:48 +0200

    Added get_date() method

--- weathload.py
+++ weathload.py
@@ -1,19 +1,31 @@
+import time
+from datetime import datetime, date
 import requests
 import json
 
-#Get Api-Key from File
+#Get Api-Key from File 
 def get_apik():
     key_file = open("weather_key.txt","r")
     key = key_file.read()
     return key
 
-def get_request(info_list,key):
+def get_date(args):
+    d = datetime.strptime(args.date,"%d/%m/%y %H:%M")
+    unixtime = time.mktime(d.timetuple())
+    print unixtime
+
+def get_request(info_list,key,args):
     lat = info_list[1]
     lng = info_list[2]
    
     key = key[:-1] 
-    response = "https://api.darksky.net/forecast/" + key + "/" + str(lat) + "," + str(lng)
-   # print resp
+   
+    if not args.date:
+       response = "https://api.darksky.net/forecast/" + key + "/" + str(lat) + "," + str(lng)
+    else:
+       response = "https://api.darksky.net/forecast/" + key + "/" + str(lat) + "," + str(lng) + "," + get_date(args)   
+
+ 
     response = requests.get("https://api.darksky.net/forecast/" + key + "/" + str(lat) +  "," + str(lng)) 
    
     return response
@@ -23,11 +35,8 @@ def get_jsond(response):
    
     return json_data	
 
-def get_weather(info_list):
+def get_weather(info_list,args):
     key = get_apik()
-    response = get_request(info_list,key)
+    response = get_request(info_list,key,args)
     data = get_jsond(response)
     return data
-
-
-
