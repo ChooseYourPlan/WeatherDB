35cdcdfadd5cc7ab0b40da05879be51e9ade8dd7
ChooseYourPlan <timweissenfels7@gmail.com>
2019-06-01 20:36:33 +0200

    Deleted old sql insert statement

--- db_query.py
+++ db_query.py
@@ -10,9 +10,7 @@ def insert_db(values):
          sql = "CREATE TABLE wetter (latitude float NOT NULL,longtitude float NOT NULL,timezone VARCHAR(60) NOT NULL, currently_summary VARCHAR(120),currently_temperature float NOT NULL ,currently_time VARCHAR(30),currently_apparentTemperature float, cuurently_humidity float, currently_pressure float, currently_windSpeed float, currently_cloudCover float, currently_visibility float);"
         
          cursor.execute(sql)
-          
-         sql = "INSERT INTO wetter (latitude, longtitude, timezone, currently_summary, currently_temperature, currently_time) VALUES ("+ str(values[0]) + "," + str(values[1]) + ",'" + values[2] + "','" + str(values[3]) + "'," + str(values[4]) + ",'" + str(values[5]) + "');"
-         
+           
          sql = "INSERT INTO wetter VALUES ("
 
          for value in values:
