c493c7aa1bceae17c4bda20283a073bad8a5951d
ChooseYourPlan <timweissenfels7@gmail.com>
2019-06-03 23:54:53 +0200

    No Currently only variables anymore

--- valuesbydata.py
+++ valuesbydata.py
@@ -8,41 +8,39 @@ def unixtimetotime(time):
 def fahrtocels(fahr):
     return (fahr - 32) / 1.8
 
-def checkjsonkey(data,loc,key):
+def checkjsonkey(data,loc,key,i = 0):
     if key in data[loc]:
         return data[loc][key]
+    elif i and key in data[loc]['data'][i]:
+        return data[loc]['data'][i][key]
     else:
         return 0
-       
-def get_values(data):
+
+def get_values(data,i = 0):
     latitude = data['latitude']
     longitude = data['longitude']
     timezone = data['timezone'] 
-
-    #currently 
-    currently_time = data['currently']['time']
-    currently_time = unixtimetotime(currently_time)
-    currently_summary = checkjsonkey(data,'currently','summary')
-    currently_icon = checkjsonkey(data,'currently','icon')
-    currently_preciptype = checkjsonkey(data,'currently','precipType')
-    currently_temperature = fahrtocels(checkjsonkey(data,'currently','temperature'))
-    currently_apparentTemperature = fahrtocels(checkjsonkey(data,'currently','apparentTemperature'))
-    currently_humidity = checkjsonkey(data,'currently','humidity')
-    currently_pressure = checkjsonkey(data,'currently','pressure')
-    currently_windSpeed = checkjsonkey(data,'currently','windSpeed')
-    currently_cloudCover = checkjsonkey(data,'currently','cloudCover',)
-    currently_visibility = checkjsonkey(data,'currently','visibility')
-    currently_ozone = checkjsonkey(data,'currently','ozone')
-
-
-    #minutely_summary = checkjsonkey(data,'minutely','summary')
-    #minutely_time = unixtimetotime(checkjsonkey(data,'minutely','time'))
     
-    values = [latitude,longitude,timezone,
-    currently_summary,currently_icon,currently_preciptype,currently_temperature,currently_time,
-    currently_apparentTemperature,currently_humidity,currently_pressure,
-    currently_windSpeed,currently_cloudCover,currently_visibility,
-    currently_ozone]
-    #minutely_summary,minutely_time]
+    if i:
+        loc = 'hourly'
+    else:
+        loc = 'currently'
+    
+    time = checkjsonkey(data,loc,'time',i)
+    time = unixtimetotime(time)
+    summary = checkjsonkey(data,loc,'summary',i)
+    icon = checkjsonkey(data,loc,'icon',i)
+    preciptype = checkjsonkey(data,loc,'precipType',i)
+    temperature = fahrtocels(checkjsonkey(data,loc,'temperature',i))
+    apparentTemperature = fahrtocels(checkjsonkey(data,loc,'apparentTemperature',i))
+    humidity = checkjsonkey(data,loc,'humidity',i)
+    dewPoint = checkjsonkey(data,loc,'dewPoint',i)
+    windSpeed = checkjsonkey(data,loc,'windSpeed',i)
+    cloudCover = checkjsonkey(data,loc,'cloudCover',i)
+    visibility = checkjsonkey(data,loc,'visibility',i)
+    uvIndex = checkjsonkey(data,loc,'uvIndex',i)
+    
+    values = [latitude,longitude,timezone,summary,icon,preciptype,temperature,time,
+    apparentTemperature,humidity,dewPoint,windSpeed,cloudCover,visibility,uvIndex]
          
     return values 
